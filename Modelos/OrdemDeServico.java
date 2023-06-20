35252a9415849b59d41b19f81988b640b10c8bd0
enzobenficaa <128191773+enzobenficaa@users.noreply.github.com>
2023-06-20 13:19:37 -0300

    Add files via upload

---
+++ Modelos/OrdemDeServico.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelos;
+public class OrdemDeServico {
+    private int idOrdemServico;
+    private Tutor tutorAnimal;
+
+    public int getIdOrdemServico() {
+        return idOrdemServico;
+    }
+
+    public void setIdOrdemServico(int idOrdemServico) {
+        this.idOrdemServico = idOrdemServico;
+    }
+
+    public Tutor getTutorAnimal() {
+        return tutorAnimal;
+    }
+
+    public void setTutorAnimal(Tutor tutorAnimal) {
+        this.tutorAnimal = tutorAnimal;
+    }
+    
+    
+}
